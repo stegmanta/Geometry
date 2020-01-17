5ca8fec165758bfe4ecb4901bc6de47558b9f86b
Tayler Allen Stegman <stegmanta@s.dcsdk12.org>
2020-01-17 08:26:54 -0700

    finished square

--- CheckTriangle.java
+++ CheckTriangle.java
@@ -11,5 +11,18 @@ public class CheckTriangle{
         
         System.out.println(b.distance(d));
         System.out.println(triangleW.area());
+
+        Point squareACorner = new Point(0, 0);
+        Square squareA = new Square(squareACorner, 10);
+
+        Point squareBCorner = new Point(5, 5);
+        Square squareB = new Square(squareBCorner, 10);
+
+        Point squareCCorner = new Point(-10, -10);
+        Square squareC = new Square(squareCCorner, 5);
+
+        System.out.println(Square.collision(squareA, squareB));
+        System.out.println(Square.collision(squareA, squareC));
+
     }
 }
