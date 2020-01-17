1c969d673a8d5b15fc5cbe679fe0cf1c6b917a57
Pierce Michael Angeloff <angeloffpm@s.dcsdk12.org>
2020-01-17 07:38:49 -0700

    Added Square Class

---
+++ Square.java
@@ -0,0 +1,22 @@
+public class Square {
+
+    private Point bottomLeftCorner;
+    private int sidelength;
+
+    // Constructor
+    public Square(Point bottomLeftCorner, int sidelength) {
+
+        this.bottomLeftCorner = bottomLeftCorner;
+        this.sidelength = sidelength;
+
+    }
+
+    public int area() {
+        return this.sidelength * this.sidelength;
+    }
+
+    public int perimeter() {
+        return this.sidelength * 4;
+    }
+
+}
