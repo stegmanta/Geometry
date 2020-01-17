b2d036998737c4096f1f56e02e93eb8383a96ff3
Tayler Allen Stegman <stegmanta@s.dcsdk12.org>
2020-01-17 08:00:43 -0700

    Fixed Square up

--- Square.java
+++ Square.java
@@ -1,13 +1,8 @@
-    
 public class Square {
 
     private Point bottomLeftCorner;
     private int sidelength;
 
-    // Determined based on bottom left corner given.
-    private Point bottomRightCorner;
-    private Point topLeftCorner;
-    private Point topRightCorner;
 
     // Constructor
     public Square(Point bottomLeftCorner, int sidelength) {
@@ -15,12 +10,6 @@ public class Square {
         this.bottomLeftCorner = bottomLeftCorner;
         this.sidelength = sidelength;
 
-        // Sets other points based on bottom left corner.
-        /**
-         * this.bottomRightCorner = new Point(bottomLeftCorner.getX() + sidelength, bottomLeftCorner.getY());
-         * this.topLeftCorner = new Point(bottomLeftCorner.getX(), bottomLeftCorner.getY() + sidelength);
-         * this.topRightCorner = new Point(bottomLeftCorner.getX() + sidelength, bottomLeftCorner.getY() + sidelength);
-        */
     }
 
     public int area() {
